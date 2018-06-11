5c1ff12b004362883bf624bcbd5c4e327c8626e7
J. Holmes <jholmes@n.io>
2018-06-11 13:11:05 -0600

    tweaking palette implementation

--- resources/palette.go
+++ resources/palette.go
@@ -4,37 +4,38 @@ import (
 	"image/color"
 )
 
-type egaColor struct {
-	Color uint16
+type rgb24Color struct {
+	RGB uint32
 }
 
-func (ega egaColor) RGBA() (r, g, b, a uint32) {
-	rn := uint32(ega.Color >> 8 & 0xf)
-	gn := uint32(ega.Color >> 4 & 0xf)
-	bn := uint32(ega.Color & 0xf)
-	r = rn<<28 | rn<<24 | rn<<20 | rn<<16 | rn<<12 | rn<<8 | rn<<4 | rn<<0
-	g = gn<<28 | gn<<24 | gn<<20 | gn<<16 | gn<<12 | gn<<8 | gn<<4 | gn<<0
-	b = bn<<28 | bn<<24 | bn<<20 | bn<<16 | bn<<12 | bn<<8 | bn<<4 | rn<<0
+func (ega rgb24Color) RGBA() (r, g, b, a uint32) {
+	rb := uint32(ega.RGB >> 16 & 0xFF)
+	gb := uint32(ega.RGB >> 8 & 0xFF)
+	bb := uint32(ega.RGB & 0xFF)
+
+	r = rb<<24 | rb<<16 | rb<<8 | rb<<0
+	g = gb<<24 | gb<<16 | gb<<8 | gb<<0
+	b = bb<<24 | bb<<16 | bb<<8 | bb<<0
 	a = 0xFFFFFFFF
 	return
 }
 
 var egaPalette = []color.Color{
-	egaColor{0x000},
-	egaColor{0x00A},
-	egaColor{0x0A0},
-	egaColor{0x0AA},
-	egaColor{0xA00},
-	egaColor{0xA0A},
-	egaColor{0xA50},
-	egaColor{0xAAA},
+	rgb24Color{0x000000},
+	rgb24Color{0x0000AA},
+	rgb24Color{0x00AA00},
+	rgb24Color{0x00AAAA},
+	rgb24Color{0xAA0000},
+	rgb24Color{0xAA00AA},
+	rgb24Color{0xAA5500},
+	rgb24Color{0xAAAAAA},
 
-	egaColor{0x555},
-	egaColor{0x55F},
-	egaColor{0x5F5},
-	egaColor{0x5FF},
-	egaColor{0xF55},
-	egaColor{0xF5F},
-	egaColor{0xFF5},
-	egaColor{0xFFF},
+	rgb24Color{0X555555},
+	rgb24Color{0X5555FF},
+	rgb24Color{0X55FF55},
+	rgb24Color{0X55FFFF},
+	rgb24Color{0XFF5555},
+	rgb24Color{0XFF55FF},
+	rgb24Color{0XFFFF55},
+	rgb24Color{0XFFFFFF},
 }
