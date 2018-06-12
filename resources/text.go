c09a091c71e99a8bc585c9062567df88635e45e0
J. Holmes <jholmes@n.io>
2018-06-11 23:10:56 -0600

    text resource parsing

---
+++ resources/text.go
@@ -0,0 +1,24 @@
+package resources
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+)
+
+func ReadText(resource *Resource) ([]string, error) {
+	var text []string
+
+	reader := bufio.NewReader(bytes.NewReader(resource.bytes))
+	for {
+		if str, err := reader.ReadString(0x00); err == io.EOF {
+			break
+		} else if err == nil {
+			text = append(text, str)
+		} else {
+			return nil, err
+		}
+	}
+
+	return text, nil
+}
