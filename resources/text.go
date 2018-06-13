141eb48a9b0a92f2263eef38863b9076670cf813
J. Holmes <jholmes@n.io>
2018-06-13 14:15:01 -0600

    trim off null

--- resources/text.go
+++ resources/text.go
@@ -14,7 +14,7 @@ func ReadText(resource *Resource) ([]string, error) {
 		if str, err := reader.ReadString(0x00); err == io.EOF {
 			break
 		} else if err == nil {
-			text = append(text, str)
+			text = append(text, str[:len(str)-1])
 		} else {
 			return nil, err
 		}
