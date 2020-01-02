ea75b29a49ae27cde307a721da2304a116997715
J. Holmes <james.holmes@pinnacol.com>
2020-01-01 22:17:08 -0700

    adding a naive implementation of a 5:6 scaler

---
+++ screen/scaler_5x6.go
@@ -0,0 +1,307 @@
+package screen
+
+import (
+	"image"
+)
+
+type Scaler5x6 struct {
+	*Ditherer
+	TextureMap
+}
+
+func (s Scaler5x6) NewPic(bounds image.Rectangle) Pic {
+	normBounds := image.Rect(0, 0, bounds.Dx(), bounds.Dy())
+	scaledBounds := image.Rect(0, 0, bounds.Dx()*5, bounds.Dy()*6)
+
+	ditherer := s.Ditherer
+	if ditherer == nil {
+		ditherer = DefaultDitherers.EGA
+	}
+
+	priorityDitherer := &Ditherer{
+		Palette:  DefaultPalettes.Depth,
+		DitherFn: noDither,
+	}
+
+	controlDitherer := &Ditherer{
+		Palette:  DefaultPalettes.EGA,
+		DitherFn: noDither,
+	}
+
+	fillBuffer := make([]uint8, bounds.Dx()*bounds.Dy())
+	for i, max := 0, len(fillBuffer); i < max; i++ {
+		fillBuffer[i] = 15
+	}
+
+	return &picLayers{
+		visual: &buffer5x6{
+			Paletted:   image.NewPaletted(scaledBounds, s.Ditherer.Palette),
+			Ditherer:   ditherer,
+			TextureMap: s.TextureMap,
+
+			fillBuffer: fillBuffer,
+			bounds:     normBounds,
+		},
+		priority: &buffer1x1{
+			Paletted: image.NewPaletted(normBounds, priorityDitherer.Palette),
+			Ditherer: priorityDitherer,
+		},
+		control: &buffer1x1{
+			Paletted: image.NewPaletted(normBounds, controlDitherer.Palette),
+			Ditherer: controlDitherer,
+		},
+	}
+}
+
+type buffer5x6 struct {
+	*image.Paletted
+	*Ditherer
+	TextureMap
+
+	fillBuffer []uint8
+	bounds     image.Rectangle
+	stack      []point
+}
+
+func (b buffer5x6) Clear(color uint8) {
+	for i, max := 0, len(b.Pix); i < max; i++ {
+		y := i / b.Stride / 320
+		x := i % b.Stride / 320
+		b.Pix[i] = b.DitherAt(x, y, color)
+	}
+}
+
+func (b buffer5x6) Image() image.Image {
+	return b.Paletted
+}
+
+func (b buffer5x6) plot(x, y int, color uint8) {
+	b.fillBuffer[b.bounds.Dx()*y+x] = b.DitherAt(x, y, color)
+
+	px, py := x*5, y*6
+	texture, ok := b.TextureMap[color]
+
+	if !ok {
+		c := b.DitherAt(x, y, color)
+		for h := 0; h < 6; h++ {
+			for w := 0; w < 5; w++ {
+				dx, dy := px+w, py+h
+				offset := dy*b.Stride + dx
+				b.Pix[offset] = c
+
+			}
+		}
+	} else {
+		yTexLen := len(texture)
+		yti := y % yTexLen
+		xTexLen := len(texture[yti])
+		xti := x % xTexLen
+		tex := texture[yti][xti]
+
+		c1, c2 := b.GetMapping(color)
+
+		for h := 0; h < 6; h++ {
+			for w := 0; w < 5; w++ {
+				dx, dy := px+w, py+h
+				offset := dy*b.Stride + dx
+				if tex[h][w] {
+					b.Pix[offset] = c1
+				} else {
+					b.Pix[offset] = c2
+				}
+
+			}
+		}
+	}
+}
+
+func (b buffer5x6) Line(x1, y1, x2, y2 int, color uint8) {
+	var (
+		left   = clampInt(0, 319, x1)
+		top    = clampInt(0, 189, y1)
+		right  = clampInt(0, 319, x2)
+		bottom = clampInt(0, 189, y2)
+	)
+
+	switch {
+	case left == right:
+		swapIf(&top, &bottom, top > bottom)
+		for y := top; y <= bottom; y++ {
+			b.plot(left, y, color)
+		}
+	case top == bottom:
+		swapIf(&right, &left, right > left)
+		for x := right; x <= left; x++ {
+			b.plot(x, top, color)
+		}
+	default:
+		// bresenham
+		dx, dy := right-left, bottom-top
+		stepX, stepY := ((dx>>15)<<1)+1, ((dy>>15)<<1)+1
+
+		dx, dy = absInt(dx)<<1, absInt(dy)<<1
+
+		b.plot(left, top, color)
+		b.plot(right, bottom, color)
+
+		if dx > dy {
+			fraction := dy - (dx >> 1)
+			for left != right {
+				if fraction >= 0 {
+					top += stepY
+					fraction -= dx
+				}
+				left += stepX
+				fraction += dy
+				b.plot(left, top, color)
+			}
+		} else {
+			fraction := dx - (dy >> 1)
+			for top != bottom {
+				if fraction >= 0 {
+					left += stepX
+					fraction -= dy
+				}
+				top += stepY
+				fraction += dx
+				b.plot(left, top, color)
+			}
+		}
+	}
+}
+
+func (b buffer5x6) Pattern(cx, cy, size int, isRect bool, isSolid bool, seed uint8, color uint8) {
+	var (
+		noiseIndex = noiseOffsets[seed]
+		width      = size*2 + 2
+		height     = size*2 + 1
+		left       = clampInt(0, 320, cx-size)
+		top        = clampInt(0, 190, cy-size)
+	)
+
+	if left+width > 320 {
+		width = 320 - left
+	}
+
+	if top+height > 190 {
+		height = 190 - top
+	}
+
+	if isRect {
+		right, bottom := left+width, top+height
+
+		for py := top; py < bottom; py++ {
+			for px := left; px < right; px++ {
+				fill := isSolid || noise[noiseIndex%len(noise)]
+				if fill {
+					b.plot(px, py, color)
+				}
+				noiseIndex++
+			}
+		}
+	} else {
+		bitmap := circleBitmaps[size]
+		size := len(bitmap)
+		for y, row := range bitmap {
+			py := top + y
+			if py >= 190 {
+				break
+			}
+
+			for x := 0; x < size; x++ {
+				px := left + x
+				if px >= 320 {
+					break
+				}
+				pixel := ((row >> (size - (x + 1))) & 1) == 1
+				if pixel {
+					fill := isSolid || noise[noiseIndex%len(noise)]
+					if fill {
+						b.plot(px, py, color)
+					}
+					noiseIndex++
+				}
+			}
+		}
+	}
+}
+
+func (b buffer5x6) isLegal(p point, legalColor uint8) bool {
+	return b.fillBuffer[p.y*b.bounds.Dx()+p.x] == legalColor
+}
+
+func (b buffer5x6) Fill(cx, cy int, legalColor uint8, color uint8) {
+	var (
+		p     point
+		stack = b.stack
+	)
+
+	// initial
+	stack = append(stack, point{cx, cy})
+
+	for len(stack) > 0 {
+		p, stack = stack[0], stack[1:]
+
+		var (
+			x, y = p.x, p.y
+		)
+
+		if !b.isLegal(p, legalColor) {
+			continue
+		}
+
+		b.plot(x, y, color)
+
+		if down := (point{x, y + 1}); down.y < 190 {
+			if b.isLegal(down, legalColor) {
+				stack = append(stack, down)
+			}
+		}
+
+		if up := (point{x, y - 1}); up.y >= 0 {
+			if b.isLegal(up, legalColor) {
+				stack = append(stack, up)
+			}
+		}
+
+		// flood right
+		for dx := x + 1; dx < 320; dx++ {
+			right := point{dx, y}
+			if !b.isLegal(right, legalColor) {
+				break
+			}
+
+			b.plot(right.x, right.y, color)
+			if down := (point{dx, y + 1}); down.y < 190 {
+				if b.isLegal(down, legalColor) {
+					stack = append(stack, down)
+				}
+			}
+			if up := (point{dx, y - 1}); up.y >= 0 {
+				if b.isLegal(up, legalColor) {
+					stack = append(stack, up)
+				}
+			}
+		}
+
+		// flood left
+		for dx := x - 1; dx >= 0; dx-- {
+			left := point{dx, y}
+			if !b.isLegal(left, legalColor) {
+				break
+			}
+
+			b.plot(left.x, left.y, color)
+			if down := (point{dx, y + 1}); down.y < 190 {
+				if b.isLegal(down, legalColor) {
+					stack = append(stack, down)
+				}
+			}
+			if up := (point{dx, y - 1}); up.y >= 0 {
+				if b.isLegal(up, legalColor) {
+					stack = append(stack, up)
+				}
+			}
+		}
+	}
+}
---
+++ screen/textures.go
@@ -0,0 +1,392 @@
+package screen
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+type TextureBlock [6][5]bool
+type Texture [][]TextureBlock
+type TextureMap map[uint8]Texture
+
+func (tm TextureMap) getTexture(pair uint8) Texture {
+	if tm == nil {
+		return DefaultTextures.Dither
+	}
+	tex := tm[pair]
+	if tex == nil {
+		return DefaultTextures.Dither
+	}
+	return tex
+}
+
+/* Helpers */
+func TextureFromTemplate(s string) (tex Texture) {
+	var lines [][]rune
+	for _, l := range strings.Split(strings.Trim(s, "\n"), "\n") {
+		lines = append(lines, []rune(strings.TrimSpace(l)))
+	}
+
+	{
+		// Validate template
+		if len(lines)%6 != 0 {
+			println(len(lines))
+			panic("invalid template height")
+		}
+
+		var expectedLineLen = -1
+		for _, line := range lines {
+			ll := len(line)
+			if ll%5 != 0 {
+				panic(fmt.Errorf("invalid template width: %d is not divisible by 5", ll))
+			}
+			if expectedLineLen == -1 {
+				expectedLineLen = ll
+			} else if expectedLineLen != ll {
+				panic(fmt.Errorf("invalid template width: %d is abnormal.", ll))
+			}
+		}
+	}
+
+	ch := len(lines) / 6
+	cw := len(lines[0]) / 5
+
+	for h := 0; h < ch; h++ {
+		var row []TextureBlock
+		for w := 0; w < cw; w++ {
+			var char TextureBlock
+			for y := 0; y < 6; y++ {
+				line := lines[h*6+y]
+				for x := 0; x < 5; x++ {
+					tr := line[w*5+x]
+					char[y][x] = tr != '_' &&
+						tr != '-' &&
+						tr != '0'
+				}
+			}
+			row = append(row, char)
+		}
+		tex = append(tex, row)
+	}
+
+	return tex
+}
+
+func RandomTexture(w, h uint, size uint) (tex Texture) {
+	w *= size
+	h *= size
+
+	for y := uint(0); y < h; y++ {
+		var row []TextureBlock
+		for x := uint(0); x < w; x++ {
+			var char TextureBlock
+			row = append(row, char)
+		}
+		tex = append(tex, row)
+	}
+
+	for y1 := uint(0); y1 < h*6; y1 += size {
+		for x1 := uint(0); x1 < w*5; x1 += size {
+			bit := rand.Float64() < 0.5
+			for y2 := uint(0); y2 < size; y2++ {
+				for x2 := uint(0); x2 < size; x2++ {
+					py := y1 + y2
+					px := x1 + x2
+					tex[py/6][px/5][py%6][px%5] = bit
+				}
+			}
+		}
+	}
+
+	return tex
+}
+
+var DefaultTextures = struct {
+	Dither  Texture
+	Dither1 Texture
+	Dither2 Texture
+	Dither3 Texture
+	Dither4 Texture
+
+	Vertical1 Texture
+	Vertical2 Texture
+	Vertical3 Texture
+	Vertical4 Texture
+
+	Horizontal1 Texture
+	Horizontal2 Texture
+	Horizontal3 Texture
+	Horizontal4 Texture
+	Horizontal5 Texture
+
+	Random1 Texture
+	Random2 Texture
+	Random3 Texture
+	Random4 Texture
+	Random5 Texture
+}{
+	Dither: TextureFromTemplate(`
+XXXXX_____
+XXXXX_____
+XXXXX_____
+XXXXX_____
+XXXXX_____
+XXXXX_____
+_____XXXXX
+_____XXXXX
+_____XXXXX
+_____XXXXX
+_____XXXXX
+_____XXXXX
+`),
+
+	Dither1: TextureFromTemplate(`
+X_X_X_X_X_
+_X_X_X_X_X
+X_X_X_X_X_
+_X_X_X_X_X
+X_X_X_X_X_
+_X_X_X_X_X
+`),
+
+	Dither2: TextureFromTemplate(`
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+__XX__XX__XX__XX__XX
+__XX__XX__XX__XX__XX
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+__XX__XX__XX__XX__XX
+__XX__XX__XX__XX__XX
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+__XX__XX__XX__XX__XX
+__XX__XX__XX__XX__XX
+`),
+
+	Dither3: TextureFromTemplate(`
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+___XXX___XXX___XXX___XXX___XXX
+___XXX___XXX___XXX___XXX___XXX
+___XXX___XXX___XXX___XXX___XXX
+`),
+
+	Dither4: TextureFromTemplate(`
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+____XXXX____XXXX____XXXX____XXXX____XXXX
+`),
+
+	Vertical1: TextureFromTemplate(`
+X_X_X_X_X_
+X_X_X_X_X_
+X_X_X_X_X_
+X_X_X_X_X_
+X_X_X_X_X_
+X_X_X_X_X_
+`),
+
+	Vertical2: TextureFromTemplate(`
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+XX__XX__XX__XX__XX__
+`),
+
+	Vertical3: TextureFromTemplate(`
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+XXX___XXX___XXX___XXX___XXX___
+`),
+
+	Vertical4: TextureFromTemplate(`
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+XXXX____XXXX____XXXX____XXXX____XXXX____
+`),
+
+	Horizontal1: TextureFromTemplate(`
+XXXXX
+_____
+XXXXX
+_____
+XXXXX
+_____
+`),
+
+	Horizontal2: TextureFromTemplate(`
+XXXXX
+XXXXX
+_____
+_____
+XXXXX
+XXXXX
+_____
+_____
+XXXXX
+XXXXX
+_____
+_____
+`),
+
+	Horizontal3: TextureFromTemplate(`
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+`),
+
+	Horizontal4: TextureFromTemplate(`
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+`),
+
+	Horizontal5: TextureFromTemplate(`
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+_____
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+_____
+_____
+_____
+_____
+_____
+`),
+
+	Random1: RandomTexture(32, 20, 1),
+	Random2: RandomTexture(16, 10, 2),
+	Random3: RandomTexture(10, 7, 3),
+	Random4: RandomTexture(8, 5, 4),
+	Random5: RandomTexture(6, 4, 5),
+}
